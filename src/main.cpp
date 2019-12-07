ef77890ddc5c591f38113672be032e3ca7938fa4
dave david <dave-poissant>
2019-12-07 18:07:19 -0500

    Import MPU6050 libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
+  Serial.println("Hello World !");
 }
